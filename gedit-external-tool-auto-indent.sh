fefae37841327101efcdfa7dcc552b4205b4936c
mrk25 <subaddiction@subaddiction.net>
2022-03-08 10:39:50 +0100

    Gedit external tool snippet instructions

--- gedit-external-tool-auto-indent.sh
+++ gedit-external-tool-auto-indent.sh
@@ -4,3 +4,8 @@ TMP_FILE_NAME=.tempvimfile;
 touch $CMD_FILE_NAME&&echo "gg=G :wq! "$TMP_FILE_NAME > $CMD_FILE_NAME&&(vim $GEDIT_CURRENT_DOCUMENT_NAME -s $CMD_FILE_NAME > /dev/null 2>/dev/null)&&rm $CMD_FILE_NAME;
 cat $TMP_FILE_NAME
 rm $TMP_FILE_NAME
+# Paste this code in a gedit external tool
+# Set external tool options
+# Input: Current document
+# Output: Replace current document
+
